fc1273523484da5fd487009f600d45217142ae62
ahadsenja <hamdanilm96@gmail.com>
2021-09-22 12:00:25 +0800

    feat: add onclik event for navbar on media queries

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,8 @@ let loginForm = document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick = () => {
   loginForm.classList.toggle('active');
 }
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+}
